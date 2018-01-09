f335e252459694c541a1f4323064090a4ef14c40
yjiangnan <yjiangnan@gmail.com>
2018-01-09 15:43:16 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='TowelStuff',
+    version='0.1',
+    packages=['rosely',],
+    license='Creative Commons',
+    long_description=open('README.txt').read(),
+)
