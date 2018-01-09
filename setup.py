0179ff06d191292dc7d6997d17aa779b65e67c65
yjiangnan <yjiangnan@gmail.com>
2018-01-09 15:44:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='TowelStuff',
+    name='Robust and sensitive tools for high-throughput data analysis',
     version='0.1',
     packages=['rosely',],
     license='Creative Commons',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
