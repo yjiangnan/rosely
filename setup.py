4ada1e7c1c3e6a558a7cae57904724e13434943e
yjiangnan <yjiangnan@gmail.com>
2018-04-18 15:52:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Robust and sensitive tools for high-throughput data analysis',
-    version='0.1',
+    version='1.0',
     packages=['rosely',],
     license='Creative Commons',
     long_description=open('README.md').read(),
