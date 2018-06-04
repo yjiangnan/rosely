193ada621374b6bec845860709e56f7c0bb732bc
yjiangnan <yjiangnan@gmail.com>
2018-06-04 17:16:29 +0200

    Add file pathwayanalysis.py

--- rosely/__init__.py
+++ rosely/__init__.py
@@ -1,15 +1,19 @@
 try:
     import imp, sys
-    from . import neutralstats, ascertainedttest, libanalysis, lowess
+    from . import neutralstats, ascertainedttest, libanalysis, lowess, pathwayanalysis
     if sys.version_info >= (3, 0):
         imp.reload(lowess)
         imp.reload(neutralstats)
         imp.reload(ascertainedttest)
         imp.reload(libanalysis)
+        imp.reload(pathwayanalysis)
     
     from .lowess import loess_fit
     from .ascertainedttest import *
     from .libanalysis import *
     from .neutralstats import *
+    from .pathwayanalysis import *
 except:
     if sys.version_info >= (3, 0): raise
+
+print('Rosely version: 1.2.1')
