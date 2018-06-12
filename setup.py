7c85d8d4f9689823c6fdd567ac9ad4f45a1dd3b8
yjiangnan <yjiangnan@gmail.com>
2018-06-12 15:25:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
+import os
 
 setup(
     name='Robust and sensitive tools for high-throughput data analysis',
     version='1.0',
     packages=['rosely',],
+    package_data={'rosely':[os.path.join('rosely', 'HelveticaCY.dfont')]},
     license='Creative Commons',
     long_description=open('README.md').read(),
 )
