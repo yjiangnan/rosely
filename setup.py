c4959eefbed2d3eb1902a81d7bb223d6abc7db50
yjiangnan <yjiangnan@gmail.com>
2018-06-12 15:41:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='Robust and sensitive tools for high-throughput data analysis',
     version='1.2.1',
     packages=['rosely',],
-    datafiles=[os.path.join('rosely', 'HelveticaCY.dfont')],
+    datafiles=['HelveticaCY.dfont'],
     license='Creative Commons',
     long_description=open('README.md').read(),
 )
