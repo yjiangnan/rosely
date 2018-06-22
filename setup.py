b3eb6e420d6954d782b98a8ffd572eb63b30542d
yjiangnan <yjiangnan@gmail.com>
2018-06-22 15:29:43 +0200

    Version change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 setup(
     name='rosely',
     description='Robust and sensitive tools for high-throughput data analysis',
-    version='1.2.1',
+    version='1.2.2',
     packages=['rosely',],
     package_dir={'rosely': 'rosely'},
     package_data={'rosely':['HelveticaCY.dfont']},
