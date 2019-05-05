0d3d3b2c0992c1de89312440c63d667091914f4c
yjiangnan <yjiangnan@gmail.com>
2019-05-06 01:43:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 import os
+
+integral_ext = Extension('rosely._integral', 
+			sources = ['src/integral_wrap.cxx', 'src/integral.cpp'],
+)
 
 setup(
     name='rosely',
     description='Robust and sensitive tools for high-throughput data analysis',
-    version='1.2.2',
+    version='1.3.0',
     packages=['rosely',],
-    package_dir={'rosely': 'rosely'},
+    package_dir={'rosely': '.'},
     package_data={'rosely':['HelveticaCY.dfont']},
     license='Creative Commons',
     long_description=open('README.md').read(),
+    ext_modules = [integral_ext],
 )
